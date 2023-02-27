a3c3e2dc65b507fa61e1ef3062f8152ede87eb1f
Riyahi <p.riyahi@azmagroup.ir>
2023-02-27 18:31:23 +0330

    added some comments

--- prsmsp/panels/kavenegar.py
+++ prsmsp/panels/kavenegar.py
@@ -9,7 +9,31 @@ class KaveNegar(ABCSmsPanel):
     #receptor = None
     #message = None
 
+    def test_panel(self):
+        """test the sms panel (connection, url, etc.)
+        in this case we used the getdate api from {kavenegar} smspanel.
+        """
+
+        url = "https://api.kavenegar.com/v1/0/utils/getdate.json"
+        resp = requests.get(url)
+
+        return resp
+
     def send_sms(self, receptor: str, message: str, api_key: str):
+        """send sms with kavenegar sms panel
+
+        Args:
+            receptor (str): the reciver of your sms,
+                            if there is many seperate them with comma (,)
+                            like: 09xxx,09xxx,09xxx.
+            message (str): your message.
+            api_key (str): this is the way that kavenegar authenticate you,
+                           they will give you this when you bought your service.
+
+        Returns:
+            _type_: _description_
+        """
+
         url = f"https://api.kavenegar.com/v1/{api_key}/sms/send.json"
 
         params = {
