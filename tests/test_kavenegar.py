3efd4d5e9ffd76d0267d792a4fe813f3a1a72f95
parsariyahi <pany.parsariyahi@gmail.com>
2023-03-11 22:42:58 +0330

    added config.json file for reading data when testing

--- tests/test_kavenegar.py
+++ tests/test_kavenegar.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from prsmsp.panels.kavenegar import KaveNegar
@@ -7,10 +8,12 @@ from prsmsp.models.response import Response
 class TestRespnoseModel(unittest.TestCase):
 
     def test_send_sms(self):
-        fake_success_response = Response(200, dict())
-        panel = KaveNegar()
-        panel_resp = panel.send_sms('09393535526', 'some', '4E3638544854644634596B315A6975556849413430307864764E4A50374B6C557572416B4C6531324841453D')
-        self.assertEqual(fake_success_response, panel_resp)
+        with open('./config.json') as file:
+            kavenegar_configs = json.load(file)["kavenegar"]
+            fake_success_response = Response(200, dict())
+            panel = KaveNegar()
+            panel_resp = panel.send_sms('09393535526', 'some', kavenegar_configs["api_key"])
+            self.assertEqual(fake_success_response, panel_resp)
 
 
 if __name__ == "__main__":
