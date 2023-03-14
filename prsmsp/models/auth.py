a66a8f545f6d86d71bc38a96876001796748e110
Riyahi <p.riyahi@azmagroup.ir>
2023-03-14 11:33:16 +0330

    added: the base of auth model related with #13

---
+++ prsmsp/models/auth.py
@@ -0,0 +1,48 @@
+class TokenAuth:
+    
+    def __init__(
+        self,
+        token
+    ) -> None:
+        self.token = token
+
+class PassAuth:
+    
+    def __init__(
+        self,
+        username,
+        password
+    ) -> None:
+        self.username = username
+        self.password = password
+
+class Auth :
+
+    def __init__(
+        self,
+        auth_type,
+        **auth_info,
+    ) -> None:
+        if auth_type == 'token':
+            token = auth_info.get('token', '')
+            if not token:
+                raise ValueError
+            self.auth = TokenAuth(token)
+            return None
+
+        elif auth_type == 'pass':
+            username = auth_info.get('username', '')
+            password = auth_info.get('password', '')
+            if not username and not password :
+                raise ValueError
+            self.auth = PassAuth(username, password)
+            return None
+
+        raise ValueError
+        
+
+    def get_auth(self):
+        if isinstance(self.auth, (TokenAuth, UPAuth)) :
+            return self.auth
+
+        return None
