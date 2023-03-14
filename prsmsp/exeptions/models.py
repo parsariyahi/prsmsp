2fcf4df090f2dff525509fc7918b91fdfc962f55
Riyahi <p.riyahi@azmagroup.ir>
2023-03-14 15:54:08 +0330

    added: some base exception #16

---
+++ prsmsp/exeptions/models.py
@@ -0,0 +1,11 @@
+class AuthTypeError(Exception) :
+    pass
+
+class AuthInfoError(Exception) :
+    pass
+
+class StatusCodeUnvalid(Exception) :
+    pass
+
+class ResponseError(Exception) :
+    pass
