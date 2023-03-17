f6140ec882f92643c20f25974d97475bb1fada50
parsariyahi <pany.parsariyahi@gmail.com>
2023-03-17 15:44:15 +0330

    refactor: except json decode for webone panel

--- prsmsp/panels/webonesms.py
+++ prsmsp/panels/webonesms.py
@@ -14,7 +14,12 @@ class WebOneSms(ABCSmsPanel):
 
     def _response_parser(self, resp):
         status_code = int(resp.status_code)
-        real_response = json.loads(resp.text)
+        try:
+            real_response = json.loads(resp.text)
+        except json.JSONDecodeError:
+            # if the json coder couldn't decode,
+            # we just put it like what is is
+            real_response = {'resp': resp.text}
 
         return Response(status_code, real_response)
 
