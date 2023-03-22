b8623232a176e15f6affd04be21ec70cc1b75478
parsariyahi <pany.parsariyahi@gmail.com>
2023-03-22 14:26:01 +0330

    built: the docs base and html files

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
