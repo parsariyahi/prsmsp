d65f39b4a3dde0f0d646101416c7ae8e7a34164d
Riyahi <p.riyahi@azmagroup.ir>
2023-04-18 16:18:13 +0330

    bug(tests): Fix niksms panel, wrong panel class bug

--- tests/panels/test_niksms.py
+++ tests/panels/test_niksms.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-from prsmsp.panels import MeliPayamak
+from prsmsp.panels import NikSms
 from prsmsp.models import Response
 
 
@@ -9,10 +9,10 @@ class TestNikSmsPanel(unittest.TestCase):
 
     def test_send_sms(self):
         with open('./config.json') as file:
-            melipayamak_configs = json.load(file)["niksms"]
+            niksms_configs = json.load(file)["niksms"]
             fake_success_response = Response(200, dict())
-            panel = MeliPayamak(melipayamak_configs["username"], melipayamak_configs["password"])
-            panel_resp = panel.send_sms('09909090900', 'some', melipayamak_configs["originator"])
+            panel = NikSms(niksms_configs["username"], niksms_configs["password"])
+            panel_resp = panel.send_sms('09909090900', 'some', niksms_configs["originator"])
             self.assertEqual(fake_success_response, panel_resp)
 
 
