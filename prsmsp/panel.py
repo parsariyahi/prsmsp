c54ff45f89826d07bab8bb48f140677a3589ed34
parsariyahi <pany.parsariyahi@gmail.com>
2023-06-30 13:06:38 +0330

    feature(panels): Add PanelFactory and Panel

--- prsmsp/panel.py
+++ prsmsp/panel.py
@@ -19,9 +19,17 @@ class PanelFactory:
         }
 
         p = PANELS.get(panel_name, None)
-
+        print(p)
         if p:
             return p
 
         raise KeyError("Panel Not Found")
 
+
+class Panel:
+
+    staticmethod
+    def initiate(panel_name: str, **auth):
+        p = PanelFactory.get(panel_name)
+
+        return p(*auth.values())
